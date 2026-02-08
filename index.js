7ae6202e6434c01b28ee8023219307ef8ac7d61d
RXT ZAHID HOSSAIN <you@example.com>
2026-02-08 22:01:20 +0800

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port =process.env.port || 3000;
 
 app.get('/user/:id', (req, res) => {
   if (!/^\d+$/.test(req.params.id)) {
